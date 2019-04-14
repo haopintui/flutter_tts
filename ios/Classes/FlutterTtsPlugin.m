324f880048456e178d0c43f76df1535fb85e9e5f
shaibaoj <wuyongjinqu@gmail.com>
2019-04-14 15:17:01 +0800

    no message

---
+++ ios/Classes/FlutterTtsPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterTtsPlugin.h"
+
+@implementation FlutterTtsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_tts"
+            binaryMessenger:[registrar messenger]];
+  FlutterTtsPlugin* instance = [[FlutterTtsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
